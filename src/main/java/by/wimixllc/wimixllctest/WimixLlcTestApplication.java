62d3d9df02ce8a42d382d3d72df0bbb1fe27892b
Anton Novosad <noobito.2011@gmail.com>
2021-12-21 18:49:55 +0300

    Initial commit

---
+++ src/main/java/by/wimixllc/wimixllctest/WimixLlcTestApplication.java
@@ -0,0 +1,13 @@
+package by.wimixllc.wimixllctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimixLlcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WimixLlcTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/wimixllc/wimixllctest/WimixLlcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package by.wimixllc.wimixllctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimixLlcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
