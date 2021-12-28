847fbde12a5dde2329ce9ff56db0fa20b2e32914
Anton Novosad <noobito.2011@gmail.com>
2021-12-28 14:23:01 +0300

    Create and add AdminService; create delete method

---
+++ src/main/java/by/wimixllc/wimixllctest/service/AdminService.java
@@ -0,0 +1,31 @@
+package by.wimixllc.wimixllctest.service;
+
+import by.wimixllc.wimixllctest.entity.User;
+import by.wimixllc.wimixllctest.exception.UserNotFoundException;
+import by.wimixllc.wimixllctest.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class AdminService {
+
+    private final UserRepository userRepository;
+
+    public AdminService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void delete(Long userId) {
+        log.info("Delete user by id");
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            userRepository.deleteById(userId);
+        } else {
+            log.error("User not found");
+            throw new UserNotFoundException("User not found");
+        }
+    }
+}
