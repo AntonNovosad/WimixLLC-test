be24be69bbba458bdc0fd0e0663dd409985806c6
Anton Novosad <noobito.2011@gmail.com>
2021-12-28 14:24:30 +0300

    change method getUsers in UserController; add methods getUsers and update in UserService

--- src/main/java/by/wimixllc/wimixllctest/controller/UserController.java
+++ src/main/java/by/wimixllc/wimixllctest/controller/UserController.java
@@ -1,14 +1,11 @@
 package by.wimixllc.wimixllctest.controller;
 
-import by.wimixllc.wimixllctest.entity.User;
+import by.wimixllc.wimixllctest.dto.UpdateUserRequestDto;
 import by.wimixllc.wimixllctest.repository.UserRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import by.wimixllc.wimixllctest.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,20 +15,26 @@ public class UserController {
 
 
     private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @GetMapping("/users")
-    Page<User> getUsers(@RequestParam Optional<Integer> page,
-                        @RequestParam Optional<String> sortBy) {
-        return userRepository.findAll(
-                PageRequest.of(
-                        page.orElse(0),
-                        10,
-                        Sort.Direction.ASC, sortBy.orElse("id")
-                )
-        );
+    ResponseEntity<?> getUsers(@RequestParam Optional<Integer> page,
+                               @RequestParam Optional<String> sortBy) {
+        return new ResponseEntity<>(userService.getUsers(page, sortBy), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable Long userId) {
+        return new ResponseEntity<>(userRepository.getById(userId), HttpStatus.OK);
+    }
+
+    @PutMapping("/user/update/{userId}")
+    public ResponseEntity<?> update(@PathVariable Long userId, UpdateUserRequestDto requestDto) {
+        return new ResponseEntity<>(userService.update(userId, requestDto), HttpStatus.OK);
     }
 }
--- src/main/java/by/wimixllc/wimixllctest/service/UserService.java
+++ src/main/java/by/wimixllc/wimixllctest/service/UserService.java
@@ -1,5 +1,8 @@
 package by.wimixllc.wimixllctest.service;
 
+import by.wimixllc.wimixllctest.dto.UpdateUserRequestDto;
+import by.wimixllc.wimixllctest.exception.UserDataException;
+import by.wimixllc.wimixllctest.exception.UserNotFoundException;
 import by.wimixllc.wimixllctest.mapper.ObjectMapper;
 import by.wimixllc.wimixllctest.dto.RegistrationRequestDto;
 import by.wimixllc.wimixllctest.dto.RegistrationResponseDto;
@@ -7,19 +10,24 @@ import by.wimixllc.wimixllctest.entity.Role;
 import by.wimixllc.wimixllctest.entity.User;
 import by.wimixllc.wimixllctest.repository.RoleRepository;
 import by.wimixllc.wimixllctest.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserService {
 
     private final ObjectMapper objectMapper;
@@ -43,17 +51,40 @@ public class UserService {
         log.info("Save user in database");
         if (userRepository.existsByEmail(request.getEmailDto())) {
             log.error("User with this email:{} already exist", request.getEmailDto());
-            throw new RuntimeException("User with this email already exist");
+            throw new UserDataException("User with this email already exist");
         }
         if (userRepository.existsByPhoneNumber(request.getPhoneDto())) {
             log.error("User with this phoneNumber:{} already exist", request.getPhoneDto());
-            throw new RuntimeException("User with this phone number already exist");
+            throw new UserDataException("User with this phone number already exist");
         }
         Role role = getRoleOrThrowException(roleName);
         User user = objectMapper.toUser(request);
         user.setUserRoles(Set.of(role));
         user.setPassword(passwordEncoder().encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public Page<User> getUsers(Optional<Integer> page, Optional<String> sortBy) {
+        return userRepository.findAll(
+                PageRequest.of(
+                        page.orElse(0),
+                        10,
+                        Sort.Direction.ASC, sortBy.orElse("id")
+                )
+        );
+    }
+
+    public User update(Long id, UpdateUserRequestDto requestDto) {
+        log.info("Update user in database");
+        if (userRepository.existsById(id)) {
+            User user = userRepository.getById(id);
+            user.setFirstName(requestDto.getFirstNameDto());
+            user.setLastName(requestDto.getLastNameDto());
+            return userRepository.save(user);
+        } else {
+            log.error("User not found in database id:{}", id);
+            throw new UserNotFoundException("User not found");
+        }
     }
 
     private Role getRoleOrThrowException(String roleName) {
