b57871c3007bd30b09aa9300163a8fe843466c35
Anton Novosad <noobito.2011@gmail.com>
2021-12-30 19:51:38 +0300

    create ChatController; create WebSocketConfiguration

---
+++ src/main/java/by/wimixllc/wimixllctest/controller/ChatController.java
@@ -0,0 +1,17 @@
+package by.wimixllc.wimixllctest.controller;
+
+import by.wimixllc.wimixllctest.entity.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @MessageMapping("/message")
+    @SendTo("/chat/messages")
+    public Message getMessages(Message message) {
+        System.out.println(message);
+        return message;
+    }
+}
