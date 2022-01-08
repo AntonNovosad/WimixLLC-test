b9aed1b19682e53305bb115d2898d7c6af9e89f3
Anton Novosad <noobito.2011@gmail.com>
2022-01-08 22:36:21 +0300

    added class messageDTO

---
+++ src/main/java/by/wimixllc/wimixllctest/dto/MessageResponseDto.java
@@ -0,0 +1,11 @@
+package by.wimixllc.wimixllctest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class MessageResponseDto {
+    private String response;
+}
--- src/main/java/by/wimixllc/wimixllctest/entity/Message.java
+++ src/main/java/by/wimixllc/wimixllctest/entity/Message.java
@@ -1,18 +1,8 @@
 package by.wimixllc.wimixllctest.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Message {
-    private String from;
     private String message;
-
-    @Override
-    public String toString() {
-        return "Message [from=" + from + ", message=" + message + "]";
-    }
 }
